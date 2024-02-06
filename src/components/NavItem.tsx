0a9cf6dc54e6f2a8b0aa66e3aa974e3de41587fb
dawa sherpa <sherpadawag1998@gmail.com>
2024-02-07 00:24:46 +0545

    woring on navbar

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { PRODUCT_CATEGORIES } from '@/config';
+import { ChevronDown } from 'lucide-react';
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
+
+type Category = (typeof PRODUCT_CATEGORIES)[number];
+interface NavItemProps {
+  category: Category;
+  handleOpen: () => void;
+  isOpen: boolean;
+  isAnyOpen: boolean;
+}
+export default function NavItem({
+  category,
+  handleOpen,
+  isOpen,
+  isAnyOpen,
+}: NavItemProps) {
+  return (
+    <div className="flex">
+      <div className="relative flex items-center">
+        <Button
+          className="gap-1.5"
+          onClick={handleOpen}
+          variant={isOpen ? 'secondary' : 'ghost'}
+        >
+          {category.label}
+          <ChevronDown
+            className={cn('h-4 w-4 transition-all text-muted-foreground', {
+              '-rotate-180': isOpen,
+            })}
+          />
+        </Button>
+      </div>
+    </div>
+  );
+}
