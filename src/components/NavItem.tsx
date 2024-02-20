1cd4dea9cfa79bf781fe75fb3e667218a19c1654
dawa sherpa <sherpadawag1998@gmail.com>
2024-02-20 23:48:40 +0545

    navbar
    used ref to closed the navbar on outide click
    create  custom hook to outside click
    used keyword to close the navbar

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -45,10 +45,10 @@ export default function NavItem({
           )}
         >
           <div
-            className="absolute inset-0 top-1/2 bg-white shadow"
+            className=" inset-0 top-1/2 bg-white shadow"
             aria-hidden="true"
           />
-          <div className="relative bg-white">
+          <div className="relative bg-white ">
             <div className="mx-auto max-w-7xl px-8">
               <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
                 <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
