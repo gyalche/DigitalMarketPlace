8e448fede9fd5b441046743edb18509265640753
dawa sherpa <sherpadawag1998@gmail.com>
2024-02-25 22:42:37 +0545

    working on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,10 @@ import MaxWidthWrapper from './MaxWidthWrapper';
 import Link from 'next/link';
 import { Icons } from './Icons';
 import NavItems from './NavItems';
+import { Button, buttonVariants } from './ui/button';
 
 export default function Navbar() {
+  const user = null;
   return (
     <div className="sticky top-0 bg-white z-50 inset-x-0 h-16">
       <header className="relative bg-white">
@@ -21,6 +23,18 @@ export default function Navbar() {
 
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
+              </div>
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {user ? null : (
+                    <Link
+                      href="/sign-in"
+                      className={buttonVariants({ variant: 'ghost' })}
+                    >
+                      Sign in
+                    </Link>
+                  )}
+                </div>
               </div>
             </div>
           </div>
